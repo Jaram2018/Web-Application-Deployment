a57b77a83e31a23c7bd292e6573f076ce783db5f
ByeongGil-Jung <jbkcose@gmail.com>
2018-10-26 16:36:31 +0900

    [18-10-26] :: init - init

---
+++ src/main/java/com/bk/webapplicationdeployment/WebApplicationDeploymentApplication.java
@@ -0,0 +1,12 @@
+package com.bk.webapplicationdeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationDeploymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplicationDeploymentApplication.class, args);
+    }
+}
