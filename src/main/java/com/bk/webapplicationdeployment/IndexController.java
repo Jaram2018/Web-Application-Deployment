8c770563373822831381b1cefee609735fce9a63
ByeongGil-Jung <jbkcose@gmail.com>
2018-10-28 00:09:47 +0900

    [18-10-28] :: (Test) Changing version 0.0.3 to 0.0.4 - (Test) Changing version 0.0.3 to 0.0.4

--- src/main/java/com/bk/webapplicationdeployment/IndexController.java
+++ src/main/java/com/bk/webapplicationdeployment/IndexController.java
@@ -17,7 +17,7 @@ public class IndexController {
 
     @GetMapping("/")
     public String version(Model model) {
-        model.addAttribute("version", "0.0.3");
+        model.addAttribute("version", "0.0.4");
         return "index";
     }
 }
