eb80bb7ae3159509245c4a881b6eb003b959091a
ByeongGil-Jung <jbkcose@gmail.com>
2018-10-27 22:24:25 +0900

    [18-10-27] :: Adding 'deploy.sh' - Adding 'deploy.sh'

---
+++ scripts/deploy.sh
@@ -0,0 +1,34 @@
+﻿APP_FOLDER=/home/ec2-user/app
+REPOSITORY=$APP_FOLDER/git
+echo "APP_FOLDER : $APP_FOLDER"
+echo "REPOSITORY : $REPOSITORY"
+
+cd $REPOSITORY/Web-Application-Deployment
+
+#!/bin/bash
+echo "> Git Pull"
+git pull
+
+echo "> Start project build"
+./gradlew build
+
+echo "> Copy build file"
+cp ./build/libs/*.jar $APP_FOLDER/
+
+echo "> Check 'pid' of the current operating application"
+CURRENT_PID=$(pgrep -f web-application-deployment)
+echo "$CURRENT_PID"
+
+if [ -z $CURRENT_PID ]; then
+	echo "> There is no operating app."
+else
+	echo "> kill -2 $CURRENT_PID"
+	kill -2 $CURRENT_PID
+	sleep 5
+fi
+
+echo "> JAR name : $JAR_NAME"
+JAR_NAME=$(ls $APP_FOLDER/ |grep 'web-application-deployment' | tail -n 1)
+
+echo "> Deploy new application"
+nohup java -jar $APP_FOLDER/$JAR_NAME &
