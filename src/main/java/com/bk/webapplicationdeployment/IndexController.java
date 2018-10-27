ae47b1f7d8e74249c1f4e16f41da3fd7a197146e
ByeongGil-Jung <jbkcose@gmail.com>
2018-10-28 02:04:06 +0900

    [18-10-28] :: (Test) Changing version 0.0.8 to 0.0.9 - (Test) Changing version 0.0.8 to 0.0.9

--- src/main/java/com/bk/webapplicationdeployment/IndexController.java
+++ src/main/java/com/bk/webapplicationdeployment/IndexController.java
@@ -17,7 +17,7 @@ public class IndexController {
 
     @GetMapping("/")
     public String version(Model model) {
-        model.addAttribute("version", "0.0.8");
+        model.addAttribute("version", "0.0.9");
         return "index";
     }
 }
