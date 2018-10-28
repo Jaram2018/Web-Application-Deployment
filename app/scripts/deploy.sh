23f29f4b37c583709d6bf062333d127841f3ce22
ByeongGil-Jung <jbkcose@gmail.com>
2018-10-28 19:31:27 +0900

    [18-10-28] :: Modifying 'deploy.sh' - Modifying 'deploy.sh'

--- app/scripts/deploy.sh
+++ app/scripts/deploy.sh
@@ -1,8 +1,9 @@
 ﻿#!/bin/bash
 
 GIT_FOLDER=/home/ec2-user/Web-Application-Deployment/
-APP_FOLDER=$GIT_FOLDER/app
-REPOSITORY=$APP_FOLDER/web-application-deployment
+APP_FOLDER=${GIT_FOLDER}/app
+REPOSITORY=${APP_FOLDER}/web-application-deployment
+JAR_PATH=${APP_FOLDER}/jar
 
 echo "APP_FOLDER : $APP_FOLDER"
 echo "REPOSITORY : $REPOSITORY"
@@ -12,28 +13,28 @@ cd /home/ec2-user/Web-Application-Deployment/
 echo "> Git Pull"
 git pull
 
-cd $REPOSITORY
+cd ${REPOSITORY}
 
 echo "> Start project build"
 ./gradlew build
 
 echo "> Copy build file"
-cp ./build/libs/*.jar $GIT_FOLDER/
+cp ./build/libs/*.jar ${JAR_PATH}/
 
 echo "> Check 'pid' of the current operating application"
 CURRENT_PID=$(pgrep -f web-application-deployment)
 echo "$CURRENT_PID"
 
-if [ -z $CURRENT_PID ]; then
+if [ -z ${CURRENT_PID} ]; then
 	echo "> There is no operating app."
 else
 	echo "> kill -15 $CURRENT_PID"
-	kill -15 $CURRENT_PID
+	kill -15 ${CURRENT_PID}
 	sleep 5
 fi
 
-JAR_NAME=$(ls $GIT_FOLDER/ |grep 'web-application-deployment' | tail -n 1)
+JAR_NAME=$(ls ${JAR_PATH}/ |grep 'web-application-deployment' | tail -n 1)
 echo "> JAR name : $JAR_NAME"
 
 echo "> Deploy new application"
-nohup java -jar $GIT_FOLDER/$JAR_NAME &
+nohup java -jar ${JAR_PATH}/${JAR_NAME} &
