853b673bddc70bdd844e0983b5d5fd1dc35b8812
ByeongGil-Jung <jbkcose@gmail.com>
2018-10-28 16:00:57 +0900

    [18-10-28] :: Classifying files and folders - Classifying files and folders

--- src/main/java/com/bk/webapplicationdeployment/WebApplicationDeploymentApplication.java
+++ src/main/java/com/bk/webapplicationdeployment/WebApplicationDeploymentApplication.java
@@ -2,14 +2,19 @@ package com.bk.webapplicationdeployment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class WebApplicationDeploymentApplication {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml, "
+            + "../config/external-config.yml";
+
     public static void main(String[] args) {
         // SpringApplication.run(WebApplicationDeploymentApplication.class, args);
-        SpringApplication app = new SpringApplication(WebApplicationDeploymentApplication.class);
-
-        app.run(args);
+        new SpringApplicationBuilder(WebApplicationDeploymentApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run();
     }
 }
