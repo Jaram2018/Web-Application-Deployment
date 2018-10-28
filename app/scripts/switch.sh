987d8a1a6a2de4375ef2ef9966a83ca03e0d9c03
ByeongGil-Jung <jbkcose@gmail.com>
2018-10-28 23:12:03 +0900

    [18-10-28] :: Modifying 'switch.sh' and the configuration path - Modifying 'switch.sh' and the configuration path

--- app/scripts/switch.sh
+++ app/scripts/switch.sh
@@ -7,14 +7,14 @@ CURRENT_PROFILE=$(curl -s http://localhost/profile)
 # 사용하지 않는 set 을 찾는다
 if [ ${CURRENT_PROFILE} == set1 ]
 then
-    IDLE_PORT=8082
+    IDLE_PORT=9082
 elif [ ${CURRENT_PROFILE} == set2 ]
 then
-    IDLE_PORT=8081
+    IDLE_PORT=9081
 else
     echo "> There is no matched profile (Profile : ${CURRENT_PROFILE})"
-    echo "> Allocated to 8081"
-    IDLE_PORT=8081
+    echo "> Allocated to 9081"
+    IDLE_PORT=9081
 fi
 
 echo "> Switching the port to : ${IDLE_PORT}"
