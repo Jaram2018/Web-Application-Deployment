80d4e398e1c5a5bc8ecdad6ea1544ca33a21c232
ByeongGil-Jung <jbkcose@gmail.com>
2018-10-29 17:51:15 +0900

    [18-10-29] :: (Test) Changing version 0.1.4 to 0.1.5 - (Test) Changing version 0.1.4 to 0.1.5

--- app/web-application-deployment/src/main/java/com/bk/webapplicationdeployment/AppController.java
+++ app/web-application-deployment/src/main/java/com/bk/webapplicationdeployment/AppController.java
@@ -24,7 +24,7 @@ public class AppController {
 
     @GetMapping("/")
     public String version(Model model) {
-        model.addAttribute("version", "0.1.4");
+        model.addAttribute("version", "0.1.5");
         return "index";
     }
 
