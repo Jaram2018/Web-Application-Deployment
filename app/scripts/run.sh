849254022d0b8870f06f1ab66ae2d417f5e75acd
ByeongGil-Jung <jbkcose@gmail.com>
2018-10-29 20:17:32 +0900

    [18-10-29] :: Modifying the 'run.sh' - Modifying the 'run.sh'

--- app/scripts/run.sh
+++ app/scripts/run.sh
@@ -21,17 +21,17 @@ echo "> Start project build"
 echo "> Copy build file"
 cp ./build/libs/*.jar ${JAR_PATH}/
 
-echo "> Check 'pid' of the current operating application"
-CURRENT_PID=$(pgrep -f web-application-deployment)
-echo "$CURRENT_PID"
-
-if [ -z ${CURRENT_PID} ]; then
-	echo "> There is no operating app."
-else
-	echo "> kill -15 $CURRENT_PID"
-	kill -15 ${CURRENT_PID}
-	sleep 5
-fi
+#echo "> Check 'pid' of the current operating application"
+#CURRENT_PID=$(pgrep -f web-application-deployment)
+#echo "$CURRENT_PID"
+#
+#if [ -z ${CURRENT_PID} ]; then
+#	echo "> There is no operating app."
+#else
+#	echo "> kill -15 $CURRENT_PID"
+#	kill -15 ${CURRENT_PID}
+#	sleep 5
+#fi
 
 JAR_NAME=$(ls ${JAR_PATH}/ |grep 'web-application-deployment' | tail -n 1)
 echo "> JAR name : $JAR_NAME"
