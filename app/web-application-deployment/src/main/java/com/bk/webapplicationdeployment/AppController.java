0108246ea171c8854515688ff93e57f3c1efa5e3
ByeongGil-Jung <jbkcose@gmail.com>
2018-10-30 21:50:34 +0900

    [18-10-30] :: (Test) Changing version 0.1.8 to 0.1.9 - (Test) Changing version 0.1.8 to 0.1.9

--- app/web-application-deployment/src/main/java/com/bk/webapplicationdeployment/AppController.java
+++ app/web-application-deployment/src/main/java/com/bk/webapplicationdeployment/AppController.java
@@ -24,7 +24,7 @@ public class AppController {
 
     @GetMapping("/")
     public String version(Model model) {
-        model.addAttribute("version", "0.1.8");
+        model.addAttribute("version", "0.1.9");
         return "index";
     }
 
