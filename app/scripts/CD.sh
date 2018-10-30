c5294c7f95ef0a7faa147b60827142c17ac3966f
ByeongGil-Jung <jbkcose@gmail.com>
2018-10-30 20:20:45 +0900

    [18-10-30] :: Modifying scripts and adding description of code - Modifying scripts and adding description of code

---
+++ app/scripts/CD.sh
@@ -0,0 +1,42 @@
+﻿#!/bin/bash
+
+# 각 directory path 지정
+GIT_FOLDER=/home/ec2-user/Web-Application-Deployment
+APP_FOLDER=${GIT_FOLDER}/app
+REPOSITORY=${APP_FOLDER}/web-application-deployment
+JAR_PATH=${APP_FOLDER}/jar
+
+echo "APP_FOLDER : $APP_FOLDER"
+echo "REPOSITORY : $REPOSITORY"
+
+cd /home/ec2-user/Web-Application-Deployment/
+
+echo "> Git Pull"
+git pull  # git 의 최신 repo 를 pull
+
+cd ${REPOSITORY}
+
+echo "> Start project build"
+./gradlew build  # Spring Boot 빌드 (아마, 실행을 위해서 755 등의 권한 설정을 해줘야 함)
+
+echo "> Copy build file"
+cp ./build/libs/*.jar ${JAR_PATH}/  # build 된 파일을 jar 폴더에 모음
+
+echo "> Check 'pid' of the current operating application"
+CURRENT_PID=$(pgrep -f web-application-deployment)  # 이미 실행중인 app 이 있을 경우, 그 pid 를 변수화
+echo "$CURRENT_PID"
+
+if [ -z ${CURRENT_PID} ]  # 만약 실행중인 app 이 없을 경우 넘어감
+then
+	echo "> There is no operating app."
+else  # 실행중인 app 이 있을 경우 그것을 kill
+	echo "> kill -15 $CURRENT_PID"
+	kill -15 ${CURRENT_PID}
+	sleep 5
+fi
+
+JAR_NAME=$(ls ${JAR_PATH}/ |grep 'web-application-deployment' | tail -n 1)  # app 을 실행시킬 최신 app(jar 파일) 이름을 변수화
+echo "> JAR name : $JAR_NAME"
+
+echo "> Deploy new application"
+nohup java -jar ${JAR_PATH}/${JAR_NAME} &  # nuhup 으로 app 을 background process 로 실행
--- app/scripts/ZDD.sh
+++ app/scripts/ZDD.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# 각 directory path 지정
 GIT_PATH=/home/ec2-user/Web-Application-Deployment
 APP_PATH=${GIT_PATH}/app
 SCRIPTS_PATH=${APP_PATH}/scripts
@@ -7,26 +8,26 @@ CONFIG_PATH=${APP_PATH}/config
 SOURCE_PATH=${APP_PATH}/web-application-deployment
 BUILD_PATH=$(ls ${SOURCE_PATH}/build/libs/*.jar)
 
-JAR_NAME=$(basename ${BUILD_PATH})  # basename : 파일 명 추출
+JAR_NAME=$(basename ${BUILD_PATH})  # basename 으로 파일 명 추출
 echo "> The name of file in build : ${JAR_NAME}"
 
 echo "> Copy the build file"
-DEPLOY_PATH=${APP_PATH}/jar/
-cp ${BUILD_PATH} ${DEPLOY_PATH}
+DEPLOY_PATH=${APP_PATH}/jar/  # jar 파일을 모아둔 곳의 path
+cp ${BUILD_PATH} ${DEPLOY_PATH}  # Spring Boot 의 jar 파일이 있는 directory 를 DEPLOY_PATH 에 복사
 
-CURRENT_PROFILE=$(curl -s http://localhost/profile)
+CURRENT_PROFILE=$(curl -s http://localhost/profile)  # Nginx 에 설정된 현재 app 의 profile 확인
 echo "> Current profile : ${CURRENT_PROFILE}"
 
 # 작동 안하는 profile 찾기
-if [ ${CURRENT_PROFILE} == set1 ]
+if [ ${CURRENT_PROFILE} == set1 ]  # 만약 set1 이 작동 중일 경우 -> profile = set2 / port = 9082 로 설정
 then
     IDLE_PROFILE=set2
     IDLE_PORT=9082
-elif [ ${CURRENT_PROFILE} == set2 ]
+elif [ ${CURRENT_PROFILE} == set2 ]  # 만약 set2 가 작동 중일 경우 -> profile = set1 / port = 9081 로 설정
 then
     IDLE_PROFILE=set1
     IDLE_PORT=9081
-else
+else  # 만약 어떤 profile 도 아닐 경우 (아마 최초 run 일 때) -> profile = set1 / port = 9081 로 설정
     echo "> There is no matched profile (Profile : ${CURRENT_PROFILE})"
     echo "> Let's set 'set1'"
     IDLE_PROFILE=set1
@@ -34,23 +35,24 @@ else
 fi
 
 echo "> Exchange 'application.jar'"
-IDLE_APPLICATION=${IDLE_PROFILE}-web-application-deployment
-IDLE_APPLICATION_PATH=${DEPLOY_PATH}${IDLE_APPLICATION}
+IDLE_APPLICATION=${IDLE_PROFILE}-web-application-deployment  # profile-app명.jar 로 새 명명
+IDLE_APPLICATION_PATH=${DEPLOY_PATH}${IDLE_APPLICATION}  # 새 명명된 파일의 절대경로
 
-ln -Tfs ${DEPLOY_PATH}${JAR_NAME} ${IDLE_APPLICATION_PATH}  # 앞에 profile 접두어 붙이고 신규 jar 심볼릭 링크
+ln -Tfs ${DEPLOY_PATH}${JAR_NAME} ${IDLE_APPLICATION_PATH}  # 새로 명명된 파일의 jar 심볼릭 링크
 
 echo "> Check the 'pid' of operating application in ${IDLE_PROFILE}"
-IDLE_PID=$(pgrep -f ${IDLE_APPLICATION})
+IDLE_PID=$(pgrep -f ${IDLE_APPLICATION})  # 배정받은 profile 로 현재 작동중인 app 의 pid 확인
 
-if [ -z ${IDLE_PID} ]
+if [ -z ${IDLE_PID} ]  # 만약 작동중이 아니라면 넘어감 (아마 그 profile 의 최초 run)
 then
     echo "> There is no current operating application"
-else
+else  # 만약 작동중이라면 그것을 kill
     echo "> kill -15 ${IDLE_PID}"
     kill -15 ${IDLE_PID}
     sleep 5
 fi
 
+# 새로 설정한 profile 의 환경변수로 app 가동
 echo "> Deploy the ${IDLE_PROFILE} ..."
 nohup java -jar -Dspring.profiles.active=${IDLE_PROFILE} ${IDLE_APPLICATION_PATH} &
 
