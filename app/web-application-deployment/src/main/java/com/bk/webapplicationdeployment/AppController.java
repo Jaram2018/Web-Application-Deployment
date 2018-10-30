9399ebf33082e3726eb065a12b060bf503287648
ByeongGil-Jung <jbkcose@gmail.com>
2018-10-31 00:31:26 +0900

    [18-10-31] :: (Test) Changing version 0.2.2 to 0.2.3 - (Test) Changing version 0.2.2 to 0.2.3

--- app/web-application-deployment/src/main/java/com/bk/webapplicationdeployment/AppController.java
+++ app/web-application-deployment/src/main/java/com/bk/webapplicationdeployment/AppController.java
@@ -24,7 +24,7 @@ public class AppController {
 
     @GetMapping("/")
     public String version(Model model) {
-        model.addAttribute("version", "0.2.2");
+        model.addAttribute("version", "0.2.3");
         return "index";
     }
 
